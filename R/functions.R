bebf8f4ba52aef30a2b9811ffc9223baf4c3c5fd
Seb Dalgarno <seb.dalgarno@gmail.com>
2022-01-11 21:19:44 -0800

    functions information works

---
+++ R/functions.R
@@ -0,0 +1,39 @@
+#' Get functions
+#'
+#' Get information on available functions.
+#'
+#' @inheritParams params
+#' @return A tibble.
+#' @family functions
+#' @export
+#' @examples
+#' \dontrun{
+#' base_url <- "https://features.hillcrestgeo.ca/"
+#' path <- "fwa"
+#' pgf_functions(
+#'  base_url = base_url, path = path)
+#' }
+pgf_functions <- function(base_url,
+                         path,
+                         user = gh_user(),
+                         verbose = FALSE,
+                         response = FALSE) {
+  chk_string(base_url)
+  chk_string(path)
+  chk_string(user)
+  chk_flag(verbose)
+  chk_flag(response)
+
+  path <- file.path(path, "functions")
+  url <- modify_url(url = base_url, path = path, query = NULL)
+
+  resp <- get_request(
+    url = url, user = user, verbose = verbose
+  )
+
+  if(response)
+    return(resp)
+
+  x <- resp$response
+  content_json(x, "functions")
+}
